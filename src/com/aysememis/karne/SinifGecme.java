b6a4d62a15fbe439db3897212586fbf7a56d94b8
aysememis <99433450+aysememis@users.noreply.github.com>
2022-10-20 12:45:51 +0300

    karne-yapilacak

---
+++ src/com/aysememis/karne/SinifGecme.java
@@ -0,0 +1,12 @@
+package com.aysememis.karne;
+
+/**
+ * @author Ayşe Memiş
+ * @since 1.0.0
+ */
+public class SinifGecme {
+
+    public static void main(String[] args) {
+        // yapılacak
+    }
+}
